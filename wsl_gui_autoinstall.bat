a5cd3a0dc2769ba66863d6e50b8ca629f95cba22
Petre Boboc <petreboboc@posteo.net>
2018-03-04 22:08:45 +0200

    Fix for 16.04 LTS

--- wsl_gui_autoinstall.bat
+++ wsl_gui_autoinstall.bat
@@ -6,9 +6,11 @@ echo LINUXTMP = "%LINUXTMP%"
 ECHO --- Running Linux installation.  You will be prompted for your Ubuntu user's password:
 REM One big long command to be absolutely sure we're not prompted for a password repeatedly
 
-echo yes ^| add-apt-repository ppa:aseering/wsl-pulseaudio > "%TMP%\script.sh"
 echo apt-get update >> "%TMP%\script.sh"
-echo apt-get -y install pulseaudio unzip >> "%TMP%\script.sh"
+echo apt-get -y install pulseaudio >> "%TMP%\script.sh"
+echo wget https://github.com/PetreBoboc/wsl_gui_autoinstall/raw/master/libpulse0.deb >> "%TMP%\script.sh"
+echo dpkg -i install libpulse0.deb >> "%TMP%\script.sh"
+echo apt-mark hold libpulse0 >> "%TMP%\script.sh"
 echo sed -i 's/; default-server =/default-server = 127.0.0.1/' /etc/pulse/client.conf >> "%TMP%\script.sh"
 echo sed -i "s$<listen>.*</listen>$<listen>tcp:host=localhost,port=0</listen>$" /etc/dbus-1/session.conf >> "%TMP%\script.sh"
 C:\Windows\System32\bash.exe -c "chmod +x '%LINUXTMP%/script.sh' ; tr -d $'\r' < '%LINUXTMP%/script.sh' | tee '%LINUXTMP%/script_clean.sh'; sudo '%LINUXTMP%/script_clean.sh'"
